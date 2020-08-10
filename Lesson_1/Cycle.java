62b1cbe960fd7c3491d9786ea0a4e7617cf9dc55
Eugene <sirius-tyrin@yandex.ru>
2020-08-10 16:02:29 +0300

    изменения Cycle

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,21 +1,24 @@
 public class Cycle {
     public static void main(String[] args) {
+        System.out.println();
         for(int i = 0; i < 20; i++) {
             System.out.println(i);
         }
+        System.out.println();
         int j = -6;
         while (j <= 6) {
             System.out.println(j);
             j += 2;
         }
+        System.out.println();
         int k = 10;
         int sum = 0;
         do {
-            if (k%2 == 0)
-            sum += k;
+            if (k % 2 == 0) {
+                sum += k;
+                }
             k++;
-        }
-        while(k <= 20);
+        } while(k <= 20);
         System.out.println(sum);
     }
 }
