37c72ebb9ae338b41743780694d5e8296b7c5ded
Eugene <sirius-tyrin@yandex.ru>
2020-08-10 14:21:58 +0300

    10/08 Implement HW_1

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,20 @@
+ public class Variable{
+ public static void main(String[] args) {
+        byte processors = 2;
+        short frequency = 2500;
+        int ramm = 8099;
+        long numb = -561241566;
+        float freq = 2.5564f;
+        double ram = 7.91;
+        char symb = 'J';
+        boolean laptop = true;
+        System.out.println("Колличество процессоров : " + processors);
+        System.out.println("Частота : " + frequency);
+        System.out.println("Оперативная память в МБ : " + ramm);
+        System.out.println("Просто номер : " + numb);
+        System.out.println("Частота в Ггц : " + freq);
+        System.out.println("Оперативная память : " + ram);
+        System.out.println("Просто символ : " + symb);
+        System.out.println("Это правда? : " + laptop);
+    }
+}
