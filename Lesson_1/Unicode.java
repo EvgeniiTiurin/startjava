b4ea0d01d79c23604c1f3cc02117940f11469688
Eugene <sirius-tyrin@yandex.ru>
2020-08-13 10:44:18 +0300

    Сокращенная форма записи

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,10 +1,9 @@
 public class Unicode {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int i = 33;
 
         while (i <= 126) {
-            char uni = (char)(i);
-            System.out.println(uni);
+            System.out.println((char) i);
             i++;
         }
     }
