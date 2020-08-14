9edf14d52dd296c75f67aa95c884dfc48444360e
Eugene <sirius-tyrin@yandex.ru>
2020-08-14 11:53:39 +0300

    геттеры и сеттеры в волках

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,23 +1,62 @@
 public class Wolf {
-    boolean isMale;
-    String name;
-    float weight;
-    int age;
-    String color;
+    private boolean isMale;
+    private String name;
+    private float weight;
+    private int age;
+    private String color;
 
-    void move() {
+    public boolean getIsMale() {
+        return isMale;
+    }
+    void setIsMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public String getName() {
+        return name;
+    }
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+    void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    void setAge(int age) {
+        if (age > 8 ) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    public void move() {
         System.out.println("Волк переместился");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сел");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("Ууууууууууууууууууууууууууу");
     }
 
-    boolean hunt() {
+    public boolean hunt() {
         System.out.println ("Волк идёт на охоту");
         return true;
     }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,17 +1,22 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf firstWolf = new Wolf();
-        firstWolf.isMale = true;
+        /*firstWolf.isMale = true;
         firstWolf.name = "Дымок";
         firstWolf.weight = 45.65f;
         firstWolf.age = 7;
-        firstWolf.color = "Grey";
+        firstWolf.color = "Grey";*/
+        firstWolf.setIsMale(true);
+        firstWolf.setName("Дымок");
+        firstWolf.setWeight(45.55f);
+        firstWolf.setAge(6);
+        firstWolf.setColor("Dark Grey");
 
-        System.out.println(firstWolf.isMale);
-        System.out.println(firstWolf.name);
-        System.out.println(firstWolf.weight);
-        System.out.println(firstWolf.age);
-        System.out.println(firstWolf.color);
+        System.out.println(firstWolf.getIsMale());
+        System.out.println(firstWolf.getName());
+        System.out.println(firstWolf.getWeight());
+        System.out.println(firstWolf.getAge());
+        System.out.println(firstWolf.getColor());
 
         firstWolf.sit();
         firstWolf.move();
