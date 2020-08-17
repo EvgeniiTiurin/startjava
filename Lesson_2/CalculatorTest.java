3e8c855294c5bffb0533102939ea7c04cba5736f
Eugene <sirius-tyrin@yandex.ru>
2020-08-17 13:49:51 +0300

    New Calculator_1

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
+        System.out.print("Введите первое число: ");
         Scanner scan = new Scanner(System.in);
         int num1 = scan.nextInt();
-        System.out.println("Введите второе число: ");
+        System.out.println("");
+        System.out.print("Введите второе число: ");
         Scanner scan1 = new Scanner(System.in);
         int num2 = scan1.nextInt();
-        System.out.println("Введите знак операции: ");
+        System.out.println("");
+        System.out.print("Введите знак операции: ");
         Scanner scan2 = new Scanner(System.in);
         char mathSign = scan2.next().charAt(0);
+        System.out.println("");
 
         Calculator.setCalculation(mathSign,num1,num2);
     }
