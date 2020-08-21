fc992e50c1f5a6cdf4dcf377335af2092b4e0fb6
Eugene <sirius-tyrin@yandex.ru>
2020-08-21 17:28:49 +0300

    калькулятор

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
-
 public class Calculator {
     private char mathSign;
     private int num1;
     private int num2;
-    Scanner scan = new Scanner(System.in, "Cp866");
 
     public int getNum1() {
         return num1;
     }
 
-    void setNum1(int num1) {
+    public void setNum1(int num1) {
         this.num1 = num1;
     }
 
@@ -18,7 +15,7 @@ public class Calculator {
         return num2;
     }
 
-    void setNum2(int num2) {
+    public void setNum2(int num2) {
         this.num2 = num2;
     }
 
@@ -26,29 +23,11 @@ public class Calculator {
         return mathSign;
     }
 
-    void setMathSign(char mathSign) {
-        switch(mathSign) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '%':
-            case '^':
-                this.mathSign = mathSign;
-                break;
-            default:
-                System.out.println("\nНезнакомая операция");
-                scanningSign();
-        }
+    public void setMathSign(char mathSign) {
+        this.mathSign = mathSign;
     }
 
-    public void scanningSign() {
-        System.out.print("\nВведите знак операции: ");
-        char mathSign = scan.next().charAt(0);
-        setMathSign(mathSign);
-    }
-
-    void calculate() {
+    public void calculate() {
         switch (mathSign) {
             case '+':
                 System.out.println("\nРезультат: " + (num1 + num2));
@@ -74,32 +53,6 @@ public class Calculator {
                 break;
             default:
                 break;
-        }
-        continueCalc();
-    }
-
-    void continueCalc() {
-        String yes = "да";
-        String no = "нет";
-        String answer;
-
-        System.out.print("\nХотите продолжить? [да/нет]: ");
-        answer = scan.next();
-
-        switch (answer) {
-            case "да":
-                System.out.print("\nВведите первое число: ");
-                num1 = scan.nextInt();
-                setNum1(num1);
-                System.out.print("\nВведите второе число: ");
-                num2 = scan.nextInt();
-                setNum2(num2);
-                scanningSign();
-                calculate();
-            case "нет":
-                break;
-            default:
-                continueCalc();
         }
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,16 +2,58 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-            Calculator calc = new Calculator();
+        Calculator calc = new Calculator();
+        startCalculation(calc);
+    }
 
-            Scanner scan = new Scanner(System.in, "Cp866");
-            System.out.print("Введите первое число: ");
-            int num1 = scan.nextInt();
-            calc.setNum1(num1);
-            System.out.print("\nВведите второе число: ");
-            int num2 = scan.nextInt();
-            calc.setNum2(num2);
-            calc.scanningSign();
-            calc.calculate();
+    public static void continueCalc(Calculator n) {
+        Scanner scan = new Scanner(System.in, "Cp866");
+        String answer;
+        System.out.print("\nХотите продолжить? [да/нет]: ");
+        answer = scan.next();
+        switch (answer) {
+            case "да":
+                startCalculation(n);
+            case "нет":
+                break;
+            default:
+                continueCalc(n);
+        }
+    }
+
+    public static void startCalculation(Calculator n) {
+        insertData(n);
+        scanningSign(n);
+        n.calculate();
+        continueCalc(n);
+    }
+
+    public static void scanningSign(Calculator n) {
+        Scanner scan = new Scanner(System.in, "Cp866");
+        System.out.print("\nВведите знак операции: ");
+        char mathSign = scan.next().charAt(0);
+        switch(mathSign) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+            case '^':
+                n.setMathSign(mathSign);
+                break;
+            default:
+                System.out.println("\nНезнакомая операция");
+                scanningSign(n);
+        }
+    }
+
+    public static void insertData(Calculator n) {
+        Scanner scan = new Scanner(System.in, "Cp866");
+        System.out.print("Введите первое число: ");
+        int num1 = scan.nextInt();
+        n.setNum1(num1);
+        System.out.print("\nВведите второе число: ");
+        int num2 = scan.nextInt();
+        n.setNum2(num2);
     }
 }
