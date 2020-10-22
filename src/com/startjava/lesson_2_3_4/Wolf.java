90a6ba3f2c866e9ad9a4ee8f00af0e960f5a2967
Eugene <sirius-tyrin@yandex.ru>
2020-10-22 14:59:27 +0300

    new

--- src/com/startjava/lesson_2_3_4/Wolf.java
+++ src/com/startjava/lesson_2_3_4/Wolf.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson_2;
+package com.startjava.lesson_2_3_4;
 
 public class Wolf {
     private boolean isMale;
--- src/com/startjava/lesson_2_3_4/WolfTest.java
+++ src/com/startjava/lesson_2_3_4/WolfTest.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson_2;
+package com.startjava.lesson_2_3_4;
 
 public class WolfTest {
     public static void main(String[] args) {
