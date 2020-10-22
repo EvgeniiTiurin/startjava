2e19536ae56cc26d9dddee141196f77e9e67c43e
Eugene <sirius-tyrin@yandex.ru>
2020-10-22 16:43:38 +0300

    Array

--- src/com/startjava/lesson_2_3_4/array/ArrayMain.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayMain.java
@@ -1,4 +1,11 @@
 package com.startjava.lesson_2_3_4.array;
 
 public class ArrayMain {
+    public static void main(String[] args) {
+        int[] numbers = new int[6];
+        float[] numbers1 = {5.6f, .5f, 4.3f};
+
+        System.out.println(numbers.length);
+        System.out.println(numbers1.length);
+    }
 }
